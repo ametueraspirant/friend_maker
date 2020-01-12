6e52e05b64baa3b6e2b6567a3eaae64dadd6565e
Amet <ametueraspirant@yahoo.com>
2020-01-12 10:30:40 -0700

    I think I understand this
    he confidently lied

---
+++ app/routing/apiroutes.js
@@ -0,0 +1,15 @@
+let friend_data = require("../data/friends");
+
+module.exports = function(app)
+{
+	app.get("/api/friends", function(req, res)
+	{
+		res.json(friends);
+	});
+
+	app.post("/api/friends", function(req, res)
+	{
+		friend_data.push(req.body);
+    	res.json(true);
+	});
+}
---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const app = express();
+var port = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+require("./app/routing/apiroutes")(app);
+require("./app/routing/htmlroutes")(app);
+
+app.listen(port, function() {
+  console.log("App listening on port: " + port);
+});
