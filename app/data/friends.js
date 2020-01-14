d53eb095c16fc6f54a64c1f6900de367e0db2b3e
Amet <ametueraspirant@yahoo.com>
2020-01-14 03:51:35 -0700

    images still buggy

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,9 +1,44 @@
 friend_table = 
 [
 	{
-		"name":"Ahmed",
-		"photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-		"scores":[5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+		name:"Homrrr",
+		photo:"../images/homie.png",
+		scores:[3, 3, 3, 3, 3, 3, 3, 3, 3, 3]
+	},
+	{
+		name:"Squeedr",
+		photo:"http://prntscr.com/qnbeiy",
+		scores:[1, 4, 1, 5, 2, 4, 1, 3, 4, 1]
+	},
+	{
+		name:"Mista Krabis",
+		photo:"http://prntscr.com/qnbf85",
+		scores:[5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
+	},
+	{
+		name:"Burt Smimps",
+		photo:"http://prntscr.com/qnbfri",
+		scores:[1, 2, 3, 4, 5, 1, 2, 3, 4, 5]
+	},
+	{
+		name:"Shagg Roberts",
+		photo:"http://prntscr.com/qnbi3p",
+		scores:[5, 2, 4, 4, 1, 2, 4, 3, 2, 1]
+	},
+	{
+		name:"Sausek",
+		photo:"http://prntscr.com/qnbicq",
+		scores:[4, 5, 1, 1, 3, 4, 2, 2, 5, 4]
+	},
+	{
+		name:"Rickard",
+		photo:"http://prntscr.com/qnbj6z",
+		scores:[5, 5, 1, 1, 5, 1, 5, 1, 5, 1]
+	},
+	{
+		name:"Steen",
+		photo:"http://prntscr.com/qnbjr2",
+		scores:[5, 2, 4, 5, 5, 1, 5, 5, 4, 4]
 	}
 ]
 
